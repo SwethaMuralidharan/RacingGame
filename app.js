94f351cae9c3e28a110b6715315a12cbe4d37343
Swetha Muralidharan <swethamuralidharan@swethas-mbp.attlocal.net>
2017-12-06 23:51:27 -0800

    typinggame created

--- app.js
+++ app.js
@@ -1,28 +1,14 @@
-// Requirements:
-//
-//  • Random letters appear in a containing <div> element, 1000px wide, at an interval of 1 second.
-//
-//  • Every 100 milliseconds, all the existing letters move right by 10px.
-//
-//  • Pressing a correct letter removes the oldest instance of that letter, and increases the score by 1 point.
-//
-//  • Pressing a letter that isn't on the page decreases the score by 1 point.
-//
-//  • Pressing Escape ends the game.
-//
-//  • If a letter gets all the way to the right-hand side of the container, the game is over.
-//
-//  • For every 20 letters that have been found, the interval of letter creation decreases by 10%.
 
 var Game = {};
-
-Game.initialize = function(letters, container_el, score_el, spacer_el, spacerWidth, letterWidth, containerWidth) {
+//let spacerWidth,letterWidth,containerWidth; -- Mistake, moves to front when the game ends.
+Game.initialize = function(letters, container_el, score_el, spacer_el,spacerWidth,letterWidth,containerWidth) {
   console.log(container_el);
   console.log(score_el);
   console.log(spacer_el);
   console.log(spacerWidth);
   console.log(letterWidth);
   console.log(containerWidth);
+
 //Initializing values
 this.letters = letters;
 this.$container = container_el;
@@ -30,9 +16,9 @@ this.$score = score_el;
 this.$spacer = spacer_el;
 
   this.currentSpacerWidth = 0;
-  this.spacerWidth = spacerWidth || 1; // requirements call for 10px, but I found that's way too much for a playable game
+  this.spacerWidth = spacerWidth || 1;
   this.letterWidth = letterWidth || 10;
-  this.containerWidth = containerWidth || 1000;
+  this.containerWidth = containerWidth || 500;
 
 
 
@@ -50,8 +36,113 @@ this.$spacer = spacer_el;
   return this;
 };
 
+Game.startTimer = function() {
+  var element = this;
+  this.timerID = setInterval(function(){
+    element.checkIfAtEnd();
+    element.checkIfLetterAddShouldAccelerate();
+    element.appendLetter();
+    element.lettersHTML();
+    element.render();
+  }, this.letterAddSpeed);
+};
 
-// DOM READY
+Game.spacerTimer = function() {
+  var element = this;
+  this.spacerTimerID = setInterval(function(){
+    element.checkIfAtEnd();
+    element.currentSpacerWidth = element.currentSpacerWidth + element.spacerWidth;
+    element.$spacer.width(element.currentSpacerWidth);
+  }, 100);
+};
+
+Game.checkIfAtEnd = function() {
+  if (this.letterWidth != 0 && this.containerWidth != 0) {
+    var widthSum = this.currentLetters.length * this.letterWidth + this.currentSpacerWidth + this.spacerWidth;
+    console.log(widthSum);
+    if(widthSum == this.containerWidth){
+      alert("Congratulations! You have Completed. Now Play again and beat your own score.!");
+    }
+    if (widthSum >= this.containerWidth) {
+      console.log("AT END");
+      this.stop();
+    }
+
+  }
+};
+Game.stop = function() {
+  clearInterval(this.timerID);
+  clearInterval(this.spacerTimerID);
+  this.trimFat();
+};
+Game.trimFat = function() {
+  var widthSum = this.currentLetters.length * this.letterWidth + this.currentSpacerWidth + this.spacerWidth;
+  if (widthSum > this.containerWidth) {
+    this.currentSpacerWidth = this.containerWidth - (this.currentLetters.length * this.letterWidth);
+    // adjust spacer width
+    this.$spacer.width(this.currentSpacerWidth);
+  }
+};
+
+Game.checkIfLetterAddShouldAccelerate = function() {
+  if (this.score != 0 && this.score % 20 == 0) {
+    this.letterAddSpeed = this.letterAddSpeed - (this.letterAddSpeed * this.letterAccelerateByPercent);
+    this.resetAddLetterTimer();
+  }
+};
+Game.resetAddLetterTimer = function() {
+  clearInterval(this.timerID);
+  this.startTimer();
+};
+Game.appendLetter = function() {
+  this.currentLetters.push(this.randomLetter());
+};
+Game.randomLetter = function() {
+  var numberLimit = this.letters.length;
+  var index = Math.floor((Math.random() * numberLimit) + 1);
+  return this.letters[index - 1];
+};
+
+Game.lettersHTML = function() {
+  var html = "";
+  $.each(this.currentLetters, function(k,v) {
+    html += "<div class='letter'>" + v.toUpperCase() + "</div>";
+  });
+  return html;
+};
+Game.render = function() {
+  this.checkIfAtEnd();
+  this.$container.html(this.lettersHTML());
+  this.$score.html(this.score);
+};
+Game.checkKey = function(key) {
+  /* Get keycode from user entered key and check if its available in the current set of letters.
+  inArray() is the jquery method which returns index. (-1 if no match).
+  */
+  var character = String.fromCharCode(key.which).toLowerCase();
+  console.log(character, this.currentLetters, $.inArray(character, this.currentLetters));
+
+  if ($.inArray(character, this.currentLetters) > -1) {
+    this.removeFromCurrentLetters(character);
+    this.increaseScore();
+  } else if ($.inArray(character, this.letters) > -1) {
+    this.decreaseScore();
+  }
+};
+Game.increaseScore = function() {
+  this.score = this.score + 1;
+};
+
+Game.decreaseScore = function() {
+  this.score = this.score - 1;
+};
+
+Game.removeFromCurrentLetters = function(character) {
+  var letterIndex = $.inArray(character, this.currentLetters);
+  if (letterIndex > -1) {
+    this.currentLetters.splice(letterIndex, 1);
+  }
+};
 
 $(document).ready(function()
 {
@@ -59,6 +150,10 @@ $(document).ready(function()
   "t","u", "v", "x", "y", "z"];
   // START
   var game = Game.initialize(lettersArray, $("#container #letters"), $("#score"), $("#container #spacer"));
+
+  $(window).on("keyup", function(event) {
+
+    game.checkKey(event);
 
 
 
